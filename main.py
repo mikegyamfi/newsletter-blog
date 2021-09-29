c38be3f45f3a31919f175bb5ceb3d16ba31856ff
Michael <michaelgyamfi696@gmail.com>
2021-09-29 14:55:52 +0000

    Removed Contact form

--- main.py
+++ main.py
@@ -210,5 +210,14 @@ def delete_post(post_id):
     return redirect(url_for('get_all_posts'))
 
 
+@app.route("/delete-comment/<int:comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment_to_delete = Comment.query.get(comment_id)
+    db.session.delete(comment_to_delete)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
